dd93e109108e5fa312f2392425265305c3a590ee
dabdelwahed <dabdelwahed@mobelite.fr>
2022-06-27 10:40:32 +0100

    Initial Commit

---
+++ QuranSwiftUI/QuranSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranSwiftUIApp.swift
+//  QuranSwiftUI
+//
+//  Created by Dorra Ben Abdelwahed on 27/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuranSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
